15e2913e8fc0eb561d32870807e0ab99330450c3
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-01 18:25:25 -0700

    (doc) Added example usage of debug.

--- examples/example.js
+++ examples/example.js
@@ -1,3 +1,7 @@
-var debug = require('line-debug');
+var debug = require('line-debug')('Example Module');
 
-debug.doSomethingCool();
+// => Module: Example Module Line: 4 -- Something you want to know about
+debug("Something you want to know about.");
+
+// => Module: Example Module Line: 7 -- Another thing you want to know about
+debug("Another thing you want to know about.");
